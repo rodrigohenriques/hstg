58db05b3aae71fe296154a7f0d153b06d9ce47d0
rodrigocotton <rsh.trabalho@gmail.com>
2014-11-13 01:18:12 -0200

    Renomeada arquivo layout

--- android/app/src/main/java/br/com/brosource/hstgbrasil/gui/NewsActivity.java
+++ android/app/src/main/java/br/com/brosource/hstgbrasil/gui/NewsActivity.java
@@ -23,7 +23,7 @@ public class NewsActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_news);
+        setContentView(R.layout.activity_news);
 
         ButterKnife.inject(this);
 
